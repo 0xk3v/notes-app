21e1895be110d5a48862cd27cd3fbf3934fbaf3d
n-kev1n <kevin_@tuta.io>
2021-12-16 19:08:05 +0100

    Added New Components

--- src/components/notes/StyledNotes.js
+++ src/components/notes/StyledNotes.js
@@ -1,10 +1,10 @@
-import styled, { keyframes } from "styled-components";
+import styled, { keyframes, css } from "styled-components";
 import { FaPlus } from "react-icons/fa";
-import { bounceInLeft, bounceInDown } from "react-animations";
+import { bounceInDown, bounceInLeft } from "react-animations";
 
 // Transitions
-const bounceAnimation = keyframes`${bounceInLeft}`;
-const bounceAnimation2 = keyframes`${bounceInDown}`;
+const bounceAnimation = keyframes`${bounceInDown}`;
+const bounceAnimation2 = keyframes`${bounceInLeft}`;
 
 export const Wrapper = styled.div`
   display: flex;
@@ -15,6 +15,7 @@ export const Wrapper = styled.div`
   height: 393px;
   border-radius: 38px;
   padding: 40px;
+  animation: 1s ${bounceAnimation};
 `;
 
 export const NotesHeader = styled.div`
@@ -22,17 +23,70 @@ export const NotesHeader = styled.div`
   justify-content: center;
   align-items: center;
   padding: 10px;
-  animation: 2s ${bounceAnimation2};
   h1 {
     text-decoration: underline;
-  }
+    transition: .5s all ease-in;
 `;
 
 export const NotesBody = styled.div`
   display: flex;
+  align-items: center;
+  width: 600px;
+  justify-content: space-between;
+`;
+
+export const NotesList = styled.div`
   flex-direction: column;
-  margin: 20px;
-  animation: 1s ${bounceAnimation};
+  display: ${({ isactive }) => (isactive ? "none" : "flex")};
+  animation: 0.9s ${bounceAnimation};
+  margin: 30px;
+`;
+
+export const NotesAdd = styled.div`
+  display: ${({ isactive }) => (isactive ? "flex" : "none")};
+  flex-direction: column;
+  transition: 0.4s all ease-in-out;
+  margin: 30px;
+  ${({ isactive }) =>
+    isactive &&
+    css`
+      animation: 0.9s ${bounceAnimation2};
+    `}
+`;
+
+export const NotesForm = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  label {
+    font-size: 2rem;
+    padding: 10px 5px;
+  }
+`;
+export const NotesInput = styled.div`
+  display: flex;
+  align-items: center;
+  width: 300px;
+  input {
+    outline: none;
+    border: none;
+    background: transparent;
+    border: 2px solid ${({ theme }) => theme.textColor};
+    width: 72%;
+    padding: 6px;
+    font-family: "Reenie Beanie", cursive;
+    font-size: 1.5rem;
+  }
+`;
+
+export const NotesSubmit = styled.button`
+  outline: none;
+  border: none;
+  background-color: ${({ theme }) => theme.textColor};
+  color: ${({ theme }) => theme.primaryColor};
+  font-weight: bold;
+  padding: 10px;
+  border: 2px solid ${({ theme }) => theme.textColor};
 `;
 
 export const NoteItem = styled.div`
@@ -55,5 +109,7 @@ export const NotesButton = styled(FaPlus)`
   font-size: 0.5rem;
   font-family: "Roboto", sans-serif;
   cursor: pointer;
-  animation: 1s ${bounceAnimation};
+  transition: 0.4s all ease-in-out;
+  transform-origin: center;
+  transform: ${({ isactive }) => isactive && "rotate(135deg)"};
 `;
