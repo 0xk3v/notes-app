360e4ebda893c3b7d69d59093b31e657bfe26936
n-kev1n <kevin_@tuta.io>
2021-12-16 19:09:21 +0100

    Added Form Component

--- src/components/notes/index.js
+++ src/components/notes/index.js
@@ -1,24 +1,47 @@
+import { useState } from "react";
 import {
   Wrapper,
   NotesHeader,
   NotesBody,
   NotesButton,
   NoteItem,
+  NotesList,
+  NotesAdd,
+  NotesForm,
+  NotesInput,
+  NotesSubmit,
 } from "./StyledNotes";
 
 const Notes = () => {
+  // Form Toggler State...
+  const [isActive, setIsActive] = useState(false);
+
+  // Function to toggle Form...
+  const toggleActive = () => setIsActive(!isActive);
+
   return (
     <Wrapper>
       <NotesHeader>
-        <h1>My Notes</h1>
+        <h1>{isActive ? "Add Note" : "My Notes"}</h1>
       </NotesHeader>
       <NotesBody>
-        <NoteItem>1. Drink Coffee</NoteItem>
-        <NoteItem>2. Learn React</NoteItem>
-        <NoteItem>3. Read a Book</NoteItem>
-        <NoteItem>4. Build an App</NoteItem>
+        <NotesList isactive={isActive}>
+          <NoteItem>1. Drink Coffee</NoteItem>
+          <NoteItem>2. Learn React</NoteItem>
+          <NoteItem>3. Read a Book</NoteItem>
+          <NoteItem>4. Build an App</NoteItem>
+        </NotesList>
+        <NotesAdd isactive={isActive}>
+          <NotesForm>
+            <label>Add A Note:</label>
+            <NotesInput>
+              <input type="text" placeholder="..." />
+              <NotesSubmit>Add</NotesSubmit>
+            </NotesInput>
+          </NotesForm>
+        </NotesAdd>
       </NotesBody>
-      <NotesButton />
+      <NotesButton onClick={toggleActive} isactive={isActive ? 1 : 0} />
     </Wrapper>
   );
 };
