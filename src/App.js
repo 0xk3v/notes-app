a3d7cf4fe32cc20bfe684670c62a6a787f3d8011
n-kev1n <kevin_@tuta.io>
2021-12-20 22:30:03 +0100

    Added Todos State

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import GlobalStyles from "./global";
 import { ThemeProvider } from "styled-components";
 import { lightTheme } from "./theme";
 import Notes from "./components/notes";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      task: "Eat pizzaa",
+    },
+  ]);
+
+  const on_add = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1;
+    const newTodos = { id, ...task };
+    setTodos([...todos, newTodos]);
+  };
+
   return (
     <ThemeProvider theme={lightTheme}>
       <>
         <GlobalStyles />
-        <Notes />
+        <Notes todos={todos} on_add={on_add} />
       </>
     </ThemeProvider>
   );
